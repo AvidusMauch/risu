197fc72e7d43c867d596f7d74fd4b477bf4a478d
Christian Mauch <cmauch@kip.uni-heidelberg.de>
2015-02-12 11:36:07 +0100

    added comments and some minor fixes

--- classes.py
+++ classes.py
@@ -6,27 +6,30 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+"""
+Class to handle parsing and processing  of the mensa dishes of the comming weeks
+"""
 class Mensa:
     day_range = 15
     dish_range = 14
     
-    def __init__(self):
+    def __init__(self): # initiate object with current date and mensa schedule
         self.__lastupdate = date.today()
         self.__dishes = self.import_dishes()
     
-    def update(self):
+    def update(self): # check if new day, and if so update mensa schedule
         if (self.__lastupdate < date.today()):
             self.__lastupdate = date.today()
             self.__dishes = self.import_dishes()
 
-    def import_dishes(self):
-        dishes = [[0 for x in range(self.dish_range)] for x in range(self.day_range)] 
+    def import_dishes(self): # get the mensa schedule and save it
+        dishes = [[0 for x in range(self.dish_range)] for x in range(self.day_range)] #firts index is for day second for dishnumber
         page = requests.get("http://www.studentenwerk.uni-heidelberg.de/speiseplan")
         tree = html.fromstring(page.text)
         mensa_feld = tree.xpath("//div[@class='collapse-content']")[0]
-        self.__day = [0 for x in range(self.day_range)] 
+        self.__day = [0 for x in range(self.day_range)] #saving formated dates as string list
         
-        for i in range(0, len(mensa_feld.xpath("table"))):
+        for i in range(0, len(mensa_feld.xpath("table"))): # parsing mensa schdule
             tages_essen = mensa_feld.xpath("table")[i]
             self.__day[i] = mensa_feld.xpath("h4")[i].text
             for j in range(0, len(tages_essen.xpath("tr"))-1):
@@ -34,19 +37,20 @@ class Mensa:
                 dishes[i][j] = dishes_buffer.xpath("td")[0].text
         return dishes
 
-    def get_dishes(self, day):
-        self.update()
+    def get_dishes(self, day): #get the dishes for a given day and return them
+        self.update() #check if new day
         garnish = self.__dishes[day][4] + ", "
-        if ((type(day) is int) and 0 <= day <=15): 
+        if ((type(day) is int) and 0 <= day <=15): #check for correct day
             for i in range(7, self.dish_range):
                 if self.__dishes[day][i] != 0:
                     garnish += self.__dishes[day][i] + ", "        
+            # retrun format: D1, D2, E main courses, garnish, Date
             return [self.__dishes[day][2],  self.__dishes[day][3], self.__dishes[day][5], self.__dishes[day][6], garnish[:-2], self.__day[day]]
         else:
             return None
 
-    def search_dish(self, dish_name):
-        self.update()
+    def search_dish(self, dish_name): #search all dishes fpr given string dish_name
+        self.update() # check if new day
         day_buffer = []
         search_pattern = "*" + dish_name + "*"
         found = False
@@ -54,7 +58,8 @@ class Mensa:
             for j in range(2, self.dish_range):
                 if fnmatch.fnmatch(str(self.__dishes[i][j]), str(search_pattern)):
                     found = True
-                    if 2 <= j <= 3:
+                    # format the output acording to mensa part
+                    if 2 <= j <= 3: 
                         day_buffer.append("Am " + self.__day[i] + " gibt es bei Aufgang D: " + self.__dishes[i][j])
                     elif 4 <= j <= 5:
                         day_buffer.append("Am " + self.__day[i] + " gibt es bei Aufgang D: " + self.__dishes[i][j])
--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+# default values
+
 admin = "Mauch"
 bot_nick = 'risu'
 default_network = 'bldviz'
@@ -19,8 +21,12 @@ ident = "mauchbot"
 realname = "kortana"
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Define  IRC Socket
 
+# initialize one Mensa opject
 mensa = Mensa()
 
+############################
+#   functions of the bot   #
+############################
 
 def connect_network(network_id, port):
     irc.connect((network_id,port)) #Connect to Server
@@ -28,12 +34,12 @@ def connect_network(network_id, port):
     irc.send('USER %s %s bla :%s\r\n' % (ident, network_id, realname)) #Send User Info to the server
     return irc
 
-def join_channel(channel_name):    
-    irc.send('JOIN %s\r\n' % channel_name) # Join the pre defined channel
+def join_channel(channel_name):     
+    irc.send('JOIN %s\r\n' % channel_name) 
     return 
 
 def leave_channel(channel_name):    
-    irc.send('PART %s\r\n' % channel_name) # Join the pre defined channel
+    irc.send('PART %s\r\n' % channel_name) 
     return 
 
 def disconnect_network():
@@ -41,29 +47,19 @@ def disconnect_network():
     irc.shutdown(socket.SHUT_RDWR)
     irc.close()
 
-
-def alles_dreck(channel_name):
-    page = requests.get("http://rolf-schneider.net/mensa/?translation=reality")
-    tree = html.fromstring(page.text)
-    meal = tree.xpath("//td[@id='meal']/text()")
-    irc.send('PRIVMSG %s :D1: %s\r\n' % (channel_name, meal[2]))
-    irc.send('PRIVMSG %s :D2: %s\r\n' % (channel_name, meal[3]))
-    irc.send('PRIVMSG %s :E:%s, %s \r\n' % (channel_name, meal[5], meal[6]))
-    irc.send('PRIVMSG %s :E beilagen: %s, %s, %s, %s\r\n' % (channel_name, meal[7], meal[8], meal[9], meal[10]))
-    return
-
-def print_dishes(day, destination):
+def print_dishes(day, destination): # prints the dishes for the given day to destination
     dishes = mensa.get_dishes(day)
-    if dishes == None:
+    if dishes == None: # if day was invalid value
         irc.send('PRIVMSG %s :Zu dumm ne Nummer richtig ein zu tippen?\r\n'% (destination))
     else:
-        irc.send('PRIVMSG %s :%s\r\n'% (destination, dishes[5]))
+        if day != 0: #skip printing of date if today
+            irc.send('PRIVMSG %s :%s\r\n'% (destination, dishes[5]))
         irc.send('PRIVMSG %s :D1: %s\r\n'% (destination, dishes[0]))
         irc.send('PRIVMSG %s :D2: %s\r\n' % (destination, dishes[1]))
         irc.send('PRIVMSG %s :E Hauptgerichte: %s, %s \r\n' % (destination, dishes[2], dishes[3]))
         irc.send('PRIVMSG %s :E Beilagen: %s\r\n' % (destination, dishes[4]))
 
-def mensa_search(dish_name, destination):
+def mensa_search(dish_name, destination): # searches all dishes for the given string and posts it if found 
     searched_dishes = mensa.search_dish(dish_name)
     if searched_dishes == None:
         irc.send('PRIVMSG %s :%s gibt es nicht in den naechsten 3 Wochen\r\n' % (destination, dish_name))
@@ -71,82 +67,105 @@ def mensa_search(dish_name, destination):
         for i in range(0, len(searched_dishes)):
             irc.send('PRIVMSG %s :%s\r\n' % (destination, searched_dishes[i]))
 
-def post_say(arg, sender):
-    if not arg.find(":"):
+def post_say(arg, sender): # takes a string of recievers and sends them the given message
+    if not arg.find(":"): # checking for wrong syntax
         irc.send('PRIVMSG %s :Wrong syntax guy\r\n' % (sender))
     else:
         targets = arg.partition(":")[0].split(",")
         message = arg.partition(":")[2]
-        for destination in targets:
+        for destination in targets: # send message to all destinations
             irc.send('PRIVMSG %s :%s\r\n' % (destination, message))
 
 
-def post_help(destination, command):
+def post_help(destination, command): #help for all avaiable commands
     if command == "help" or command == "!help":
-        irc.send('PRIVMSG %s :Avaiable commands: !mensa, !mensa_s, !say. Type !help "command" for more information\r\n' % (destination)) 
+        irc.send('PRIVMSG %s :Avaiable commands: !mensa, !mensasearch, !say. Type !help "command" for more information\r\n' % (destination)) 
     elif command == "mensa" or command == "!mensa":
         irc.send('PRIVMSG %s :Prints the dishes of a given day. Default day is 0 (today). Syntax: "!mensa d" d between 0 and 14.\r\n' % (destination)) 
-    elif command == "mensa_s" or command == "!mensa_s":
+    elif command == "mensasearch" or command == "!mensasearch":
         irc.send('PRIVMSG %s :Searches for the given name in dishes of the comming two weeks and prints the date and dish. Syntax: "!mensa_s name" \r\n' % (destination)) 
     elif command == "say" or command == "!say":
         irc.send('PRIVMSG %s :Syntax: "nick1,nick2,#channel1,#channel2:Hello World". Sends to the chosen targets seperated with commas the given string behind :\r\n' % (destination)) 
     else:
-        irc.send('PRIVMSG %s :Avaiable commands: !mensa, !mensa_s, !say\r\n' % (destination)) 
+        irc.send('PRIVMSG %s :Avaiable commands: !mensa, !mensasearch, !say\r\n' % (destination)) 
+
+# initialice communication
 
 connect_network(default_network, default_port)
 join_channel(default_chan)
 
-while True: #While Connection is Active
+#######################
+#   Main while loop   #
+#######################
+
+while True: 
     data = irc.recv(4096) #Make Data the Receive Buffer
     print data #Print the Data to the console(For debug purposes)
-    if data.find('PING') != -1: #If PING is Found in the Data
-        irc.send('PONG ' + data.split()[1] + '\r\n') #Send back a PONG
-
-    elif data.find('PRIVMSG') != -1: #IF PRIVMSG is in the Data Parse it
-        message = ':'.join(data.split (':')[2:]) #Split the command from the message
+    
+    if data.find('PING') != -1: #Reply to ping 
+        irc.send('PONG ' + data.split()[1] + '\r\n') 
+    
+    elif data.find('PRIVMSG') != -1: # checking for human communication
+        #message = ':'.join(data.split (':')[2:]) #Split the command from the message
+        message = data.split (':',2)[2] #Split the message from the data
         destination = data.split()[2]
-        user_nick = data.split('!')[ 0 ].replace(':','') #The nick of the user issueing the command is taken from the hostname
+        user_nick = data.split('!')[0].replace(':','') #The nick of the user issueing the command 
         function = message.split( )[0] #The function is the message split
-        arg = None
         if destination == bot_nick:
             destination = user_nick
+        arg = None #reset arg for every loop 
         if len(message.split())>1:
             arg = message.split(" ", 1)[1]
         print 'Funtion is ' + function + ' From ' + user_nick #Print who commanded [This is for debug and logging]
+        
+#################################
+#  Parsing of all bot commands  #
+#################################
+
         if (function[0] == "!"):
-            if function == "!quit" and user_nick == admin:
+            
+            if function == "!quit" and user_nick == admin: # disconnects the bot from the network
                 disconnect_network()
                 break
-            if function == "!mensa":
+            
+            elif function == "!mensa": # posts mensa dishes for given day
                 if not arg:
                     print_dishes(0, destination)
                 else:
                     print_dishes(int(arg.split()[0]), destination)
-            if function == "!mensa_s":
+            
+            elif function == "!mensasearch": #search for a sting in all dishes of the comming two weeks
                 if not arg:
                     irc.send('PRIVMSG %s : Musst schon nen Suchbegriff angeben\r\n' % (destination))
                 else:
                     mensa_search(arg.split()[0], destination)
             
-            if function == "!help":
+            elif function == "!help":
                 if not arg:
                     post_help(destination,"help")
                 else:
                     post_help(destination, arg.split()[0])
-            if function == "!say":
+            
+            elif function == "!say": #post message to list of destinations
                 if not arg:
                     irc.send('PRIVMSG %s : ????\r\n' % (destination))
                 else:
                     post_say(arg, user_nick)
-            if function ==  "!join" and user_nick == admin:
+            
+            elif function ==  "!join" and user_nick == admin: #join channel
                 channel_name = arg.split()[0] 
                 if channel_name[0] != "#":
                     join_channel("#"+channel_name)
                 else:
                     join_channel(channel_name)
-            if function ==  "!leave" and user_nick == admin:
+            
+            elif function ==  "!leave" and user_nick == admin: #leave channel
                 channel_name = arg.split()[0] 
                 if channel_name[0] != "#":
                     leave_channel("#"+channel_name)
                 else:
                     leave_channel(channel_name)
+            
+            else: #print help if unknown command was issued
+                irc.send('PRIVMSG %s :unknown command.\r\n' % (destination))
+                post_help(destination,"help")
